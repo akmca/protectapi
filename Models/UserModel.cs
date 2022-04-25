f9f1e529f8d2376f9f16f4c9c85bf3dcbdabd658
akmca <arunkumar.mca@outlook.com>
2022-04-25 20:34:37 +0530

    test

---
+++ Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProtectAPI.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
